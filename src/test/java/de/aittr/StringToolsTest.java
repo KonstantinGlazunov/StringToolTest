7bd1291dd5e330536df9013d3edc1c599e33f414
Konstantin Glazunov <kglaz@ya.ru>
2023-05-22 22:53:36 +0200

    initial commit

---
+++ src/test/java/de/aittr/StringToolsTest.java
@@ -0,0 +1,24 @@
+package de.aittr;
+
+import jdk.jfr.Name;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class anyString extends StringTools{
+    
+}
+class StringToolsTest {
+
+    @Test
+    @Name("test FirstToUpperCase")
+    void firstToUpperCase() {
+String actual =
+
+    }
+
+    @Test
+    void isStartingFromCapital() {
+    }
+}
